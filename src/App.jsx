f9f00684c845a61fc05efa9f73078b829e4a29af
Freeman Lee <lokofreeman@gmail.com>
2026-02-11 04:10:33 +0900

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,9 @@
+function App() {
+  return (
+    <div style={{ textAlign: 'center', marginTop: '50px' }}>
+      <h1>나의 첫 리액트 배포</h1>
+      <p>Github Pages에 올리기 성공!</p>
+    </div>
+  );
+}
+export default App;
